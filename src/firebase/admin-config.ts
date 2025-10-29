74fb52ceb410994c370195f5c426a216d9ed4453
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-29 20:49:01 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDQYCjCFpvW-W_eXr2OPRoLvAXNGKmD

--- src/firebase/admin-config.ts
+++ src/firebase/admin-config.ts
@@ -1,12 +1,12 @@
-// TODO: Preencha este arquivo com as credenciais do seu SEGUNDO projeto Firebase (o de Admin).
+// As credenciais para o seu SEGUNDO projeto Firebase (o de Admin).
 // Estas credenciais são usadas para se conectar ao banco de dados que contém
 // os dados de escolas, cantinas e produtos.
 
 export const firebaseAdminConfig = {
-  projectId: "COLOQUE-SEU-PROJECT-ID-ADMIN-AQUI",
-  appId: "COLOQUE-SEU-APP-ID-ADMIN-AQUI",
-  apiKey: "COLOQUE-SEU-API-KEY-ADMIN-AQUI",
-  authDomain: "COLOQUE-SEU-AUTH-DOMAIN-ADMIN-AQUI",
-  storageBucket: "COLOQUE-SEU-STORAGE-BUCKET-ADMIN-AQUI",
-  messagingSenderId: "COLOQUE-SEU-MESSAGING-SENDER-ID-ADMIN-AQUI",
+  apiKey: "AIzaSyDQYCjCFpvW-W_eXr2OPRoLvAXNGKmDXb4",
+  authDomain: "studio-2048412062-f267f.firebaseapp.com",
+  projectId: "studio-2048412062-f267f",
+  storageBucket: "studio-2048412062-f267f.appspot.com",
+  messagingSenderId: "227001608268",
+  appId: "1:227001608268:web:c10d67ea5182c02331ace9"
 };
