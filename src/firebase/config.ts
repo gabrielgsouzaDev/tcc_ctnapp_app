6a5b6524d09a8193f7a77f3451f8208f48441d54
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-29 20:50:12 +0000

    credenciais cliente: const firebaseConfig = {   apiKey: "AIzaSyA1TTmel-l

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1780973414-c81ce",
-  "appId": "1:1047852794873:web:a166890fe404df18977328",
-  "apiKey": "AIzaSyA1TTmel-lC9RQuoH6Lzch0ZJr7z4RuJfA",
-  "authDomain": "studio-1780973414-c81ce.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1047852794873"
+  apiKey: "AIzaSyA1TTmel-lC9RQuoH6Lzch0ZJr7z4RuJfA",
+  authDomain: "studio-1780973414-c81ce.firebaseapp.com",
+  projectId: "studio-1780973414-c81ce",
+  storageBucket: "studio-1780973414-c81ce.appspot.com",
+  messagingSenderId: "1047852794873",
+  appId: "1:1047852794873:web:a166890fe404df18977328"
 };
