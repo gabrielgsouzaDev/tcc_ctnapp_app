4f55337e0b5aefebae2beed8b5369fb9e3b4550e
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-30 02:50:44 +0000

    mude o icon do app para algo que faça sentido e tente usar cores que rem

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,5 +1,6 @@
 
 import { cn } from "@/lib/utils";
+import { UtensilsCrossed } from "lucide-react";
 import Link from "next/link";
 
 
@@ -7,6 +8,7 @@ export function Logo({ isSidebar = false }: { isSidebar?: boolean }) {
   if (isSidebar) {
     return (
         <Link href="/" className="flex items-center gap-2">
+            <UtensilsCrossed className="h-6 w-6 text-primary" />
             <span className="font-subtitle text-2xl font-bold text-primary">Bemmu.</span>
         </Link>
     )
@@ -15,7 +17,10 @@ export function Logo({ isSidebar = false }: { isSidebar?: boolean }) {
   return (
     <div className="text-center">
       <Link href="/" className="flex flex-col items-center">
-          <h1 className="font-subtitle text-6xl font-bold text-primary leading-none">Bemmu.</h1>
+          <div className="flex items-center justify-center gap-4">
+            <UtensilsCrossed className="h-12 w-12 text-primary" />
+            <h1 className="font-subtitle text-6xl font-bold text-primary leading-none">Bemmu.</h1>
+          </div>
         <p className="text-muted-foreground mt-2 text-center">A cantina na palma da sua mão.</p>
       </Link>
     </div>
