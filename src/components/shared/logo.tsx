bceb682041b5a28b77db094d0b651dd3fc7da75a
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-30 02:53:38 +0000

    a paleta ainda tá a mesma, quero a plaeta de cores ANTIGA e quero que TI

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,6 +1,5 @@
 
 import { cn } from "@/lib/utils";
-import { UtensilsCrossed } from "lucide-react";
 import Link from "next/link";
 
 
@@ -8,7 +7,6 @@ export function Logo({ isSidebar = false }: { isSidebar?: boolean }) {
   if (isSidebar) {
     return (
         <Link href="/" className="flex items-center gap-2">
-            <UtensilsCrossed className="h-6 w-6 text-primary" />
             <span className="font-subtitle text-2xl font-bold text-primary">Bemmu.</span>
         </Link>
     )
@@ -18,7 +16,6 @@ export function Logo({ isSidebar = false }: { isSidebar?: boolean }) {
     <div className="text-center">
       <Link href="/" className="flex flex-col items-center">
           <div className="flex items-center justify-center gap-4">
-            <UtensilsCrossed className="h-12 w-12 text-primary" />
             <h1 className="font-subtitle text-6xl font-bold text-primary leading-none">Bemmu.</h1>
           </div>
         <p className="text-muted-foreground mt-2 text-center">A cantina na palma da sua mão.</p>
